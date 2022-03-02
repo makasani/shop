ebf1012a18a4748bf21c42f5dfd29befacdef6b5
Sergey Kashurin <sergey-kashurin@mail.ru>
2022-03-02 22:54:00 +0300

    1. Исправил работу скрипта для отображения раскрывающегося списка в верхнем меню

--- assets/section-app.js
+++ assets/section-app.js
@@ -1,9 +1,10 @@
 import './css/fontawesome-all.min.css';
 import './css/main.css';
 
-require('./js/breakpoints.min');
 require('./js/browser.min');
 require('./js/jquery.dropotron.min');
 require('./js/main');
+require('./js/breakpoints.min');
 require('./js/util');
+
 
