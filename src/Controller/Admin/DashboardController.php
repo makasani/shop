2bfa136380ea3a5a2b674ca81f7e66a9e5c366d7
Viktor Chegodaev <vitas1648@gmail.com>
2022-03-06 14:49:20 +0500

    Добавлен CRUD контроллер Product

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -3,6 +3,7 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Category;
+use App\Entity\Product;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
@@ -30,6 +31,7 @@ class DashboardController extends AbstractDashboardController
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
         yield MenuItem::section('Data');
         yield MenuItem::linkToCrud('Category', 'fas fa-list', Category::class);
+        yield MenuItem::linkToCrud('Product', 'fas fa-list', Product::class);
         yield MenuItem::section('Users');
         // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
     }
